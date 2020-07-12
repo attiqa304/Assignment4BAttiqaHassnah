8b777f07d2f1cba638236309d13e78e8d04c5289
attiqa304 <fa19-rcs-013@student.comsats.edu.pk>
2020-07-13 02:45:13 +0500

    done

---
+++ HummingBirds/Assets/Scenes/Game menu/scripts/GameMenuController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameMenuController : MonoBehaviour
+{
+    public void ClickOnPenguin()
+    {
+        SceneManager.LoadScene("Penguins");
+    }
+    public void ClickOnHummingbird()
+    {
+        SceneManager.LoadScene("FlowerIsland");
+    }
+}
+
